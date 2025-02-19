fdcbcb4980b5a25cd5c284111dbb52f082bf6b41
Marcelo Fong <fonguim@hotmail.com>
2025-02-19 17:50:31 -0300

    update fixtures

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,19 @@
+import userData from '../fixtures/user-data.json'
+
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = { 
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    buttonField: '.oxd-button'
+  }
+
+
   it('Login successful', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.buttonField).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard') //verificar se o texto está correto para nao usar mesma classe
   })
